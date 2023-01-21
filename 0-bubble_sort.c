94fb492ebcd4e050f162129568bcd70f3430bb03
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-21 09:38:10 +0000

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * Bubble_sort- sort an array of integers in ascending order
+ * @array: the list of integers given
+ * @size: size of integers given
+ *
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i,j;
+	int temp;
+
+	for(i=0; i<size; i++)
+	{
+		for(j=0; j<(size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -4,10 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*print helper functions */
-
-void print_array(const int *array, size_t size)
-void print_list(const listint_t *list)
 
 /* Data structure */
 
@@ -18,12 +14,19 @@ void print_list(const listint_t *list)
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
     const int n;
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+/*print helper functions */
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
 
 /* sortint algorithms */
 void bubble_sort(int *array, size_t size);
