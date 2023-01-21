b86575f59c344f857420f100c9dc17eb1009659a
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-21 20:55:03 +0000

    selection sorting

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - an array of intergers to be sorted using selection sorting
+ * @array: list of integers to be sorted
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j <= size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != j)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -31,6 +31,7 @@ void print_list(const listint_t *list);
 /* sortint algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 
 #endif /* SORT_H */
