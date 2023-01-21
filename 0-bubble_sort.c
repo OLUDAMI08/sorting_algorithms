820991a5f7043e38615d9e5a04f845b8a6751f6b
lawal toheeb damilare <lawaltd088@gmail.com>
2023-01-21 10:05:23 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,19 +12,25 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, tracker;
 
 	for (i = 0; i < size; i++)
 	{
+		int traker = 0;
+
 		for (j = 0; j < (size - i - 1); j++)
 		{
+
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+				tracker = 1;
 			}
 		}
+		if (tracker == 0)
+			break;
 	}
 }
